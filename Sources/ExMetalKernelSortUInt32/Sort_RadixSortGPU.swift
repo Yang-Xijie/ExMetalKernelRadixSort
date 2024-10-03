5c16b46487aa9f25dc9225adcbe9f7a675499507
YangXijie <564197835@qq.com>
2024-10-03 14:36:28 +0800

    use metal_command_encoder instead of swift loop for a single GPU call

--- Sources/ExMetalKernelSortUInt32/Sort_RadixSortGPU.swift
+++ Sources/ExMetalKernelSortUInt32/Sort_RadixSortGPU.swift
@@ -11,47 +11,28 @@ let metal_library = try! metal_device.makeDefaultLibrary(bundle: Bundle.module)
 let metal_function = metal_library.makeFunction(name: "radix_sort_2bit")!
 let metal_compute_pipeline_state = try! metal_device.makeComputePipelineState(function: metal_function)
 
-func Sort_RadixSortGPU(array: MTLBuffer) {
-    Sort_RadixSortGPU(array: array, radix_2bit_start: 2 * 0)
-    Sort_RadixSortGPU(array: array, radix_2bit_start: 2 * 1)
-    Sort_RadixSortGPU(array: array, radix_2bit_start: 2 * 2)
-    Sort_RadixSortGPU(array: array, radix_2bit_start: 2 * 3)
-    Sort_RadixSortGPU(array: array, radix_2bit_start: 2 * 4)
-    Sort_RadixSortGPU(array: array, radix_2bit_start: 2 * 5)
-    Sort_RadixSortGPU(array: array, radix_2bit_start: 2 * 6)
-    Sort_RadixSortGPU(array: array, radix_2bit_start: 2 * 7)
-    Sort_RadixSortGPU(array: array, radix_2bit_start: 2 * 8)
-    Sort_RadixSortGPU(array: array, radix_2bit_start: 2 * 9)
-    Sort_RadixSortGPU(array: array, radix_2bit_start: 2 * 10)
-    Sort_RadixSortGPU(array: array, radix_2bit_start: 2 * 11)
-    Sort_RadixSortGPU(array: array, radix_2bit_start: 2 * 12)
-    Sort_RadixSortGPU(array: array, radix_2bit_start: 2 * 13)
-    Sort_RadixSortGPU(array: array, radix_2bit_start: 2 * 14)
-    Sort_RadixSortGPU(array: array, radix_2bit_start: 2 * 15)
-}
-
 // TODO: change 1024 to variable
-/// `radix`: [0,31]
-func Sort_RadixSortGPU(array: MTLBuffer, radix_2bit_start: Int) {
+func Sort_RadixSortGPU(array: MTLBuffer) {
     let metal_command_buffer = metal_command_queue.makeCommandBuffer()!
 
     let metal_command_encoder = metal_command_buffer.makeComputeCommandEncoder()!
     metal_command_encoder.setComputePipelineState(metal_compute_pipeline_state)
 
     metal_command_encoder.setBuffer(array, offset: 0, index: 0)
-    var radix_2bit_start = radix_2bit_start
-    metal_command_encoder.setBytes(&radix_2bit_start, length: MemoryLayout<UInt32>.stride, index: 10)
+    for i_radix_2bit_start in 0 ..< 16 {
+        var radix_2bit_start = i_radix_2bit_start * 2
+        metal_command_encoder.setBytes(&radix_2bit_start, length: MemoryLayout<UInt32>.stride, index: 10)
 
-    metal_command_encoder.dispatchThreads(
-        .init(
-            width: 1024,
-            height: 1,
-            depth: 1),
-        threadsPerThreadgroup: .init(
-            width: 1024,
-            height: 1,
-            depth: 1))
-
+        metal_command_encoder.dispatchThreads(
+            .init(
+                width: 1024,
+                height: 1,
+                depth: 1),
+            threadsPerThreadgroup: .init(
+                width: 1024,
+                height: 1,
+                depth: 1))
+    }
     metal_command_encoder.endEncoding()
 
     metal_command_buffer.addCompletedHandler { command_buffer in
